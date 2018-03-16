202ef515aa18c1adca246522ae9ac5feea14439c
bryndav <d.bryngelsson@gmail.com>
2018-03-16 22:56:28 +0100

    Added fabfiel featues

--- fabfile.py
+++ fabfile.py
@@ -1,13 +1,48 @@
 from fabric.api import *
 
 env.use_ssh_config = True
+env.hosts = 'bob'
 
 def production():
-    env.host = ['bob']
+    env.hosts = ['bob']
 
 def prepare_deploy():
-    local("git add -p && git commit")
-    local("git push")
+    local('git add -p && git commit')
+    local('git push')
+
+def clone():
+    with cd('/var/www'):
+        run('git clone https://github.com/bryndav/DjangoManta.git myproject')
+
+def checkout(branch):
+    with cd('/var/www/myproject'):
+        run('git checkout %s' % branch)
+
+def pull():
+    with cd('/var/www/myproject'):
+        run('git pull')
+
+def migrate():
+    with cd('/var/www/myproject'):
+        run('python manage.py migrate')
+
+def start():
+    run('/var/www/myproject/apache2/start')
+
+def stop():
+    run('/var/www/myproject/apache2/stop')
+
+def restart():
+    run('/var/www/myproject/apache2/restart')
+
+def static():
+   with cd('var/www/myproject'):
+       run('python manage.py collectstatic --noinput')
+
+#TODO fix these commands
+def update_host():
+    sudo('apt-get update')
+    sudo('apt-get upgrade -y')
 
 def deploy():
-    run('whoami')
+    run('pwd')
