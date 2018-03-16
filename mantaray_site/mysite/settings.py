578fa0248fbb5bd57f09bd08c99c0bba469295c8
bryndav <d.bryngelsson@gmail.com>
2018-03-16 22:56:49 +0100

    Removed secret

--- mantaray_site/mysite/settings.py
+++ mantaray_site/mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'kz2o^*ez*3zbtt#!k7p6=4w+*ab0$iubj)7tt)=t0fvwhy0t!s'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
